241b666a5a884b86e1a4f4767a92f2fb12e3edd7
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-06-14 01:31:21 +0530

    added validation on signUp

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "../Styles/Signup.css";
 import axios from "axios";
+import { CgDanger } from "react-icons/cg";
 import { ThreeDots } from "react-loader-spinner";
+import signUpValidation from "../schemas/signupValidation";
 function Signup() {
   const [loading, setLoading] = useState(false);
   const [signupData, setSignupData] = useState({
@@ -10,12 +12,14 @@ function Signup() {
     email: "",
     password: "",
   });
+  const [errors, setErrors] = useState({});
   function handleChange(event) {
     const { name, value } = event.target;
     setSignupData((prevValue) => ({
       ...prevValue,
       [name]: value,
     }));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
   }
   const signup = async (config) => {
     try {
@@ -27,7 +31,18 @@ function Signup() {
       console.log(error);
     }
   };
-  function handleSubmit(e) {
+  const handleBlur = (event) => {
+    const { name } = event.target;
+    try {
+      signUpValidation.validateSyncAt(name, signupData);
+    } catch (validationError) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: validationError.message,
+      }));
+    }
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
     let config = {
       method: "post",
@@ -38,40 +53,90 @@ function Signup() {
       },
       data: signupData,
     };
-    signup(config);
-  }
+    try {
+      setLoading(true);
+      await signUpValidation.validate(signupData, { abortEarly: false });
+      setLoading(false);
+      signup(config);
+    } catch (validationError) {
+      setLoading(false);
+      console.log(validationError);
+      const newErrors = {};
+      validationError.inner.forEach((error) => {
+        newErrors[error.path] = error.message;
+      });
+
+      setErrors(newErrors);
+    }
+  };
   return (
     <div className="fourGearSignUp">
       <div className="fourGearSignUpForm">
         <h2>SignUp</h2>
         <div className="fourGearSignUpFormContainer">
           <form onSubmit={handleSubmit}>
-            <input
-              name="fname"
-              placeholder="First Name"
-              onChange={handleChange}
-              value={signupData.firstName}
-            ></input>
-            <input
-              name="lname"
-              placeholder="Last Name"
-              onChange={handleChange}
-              value={signupData.lastName}
-            ></input>
-            <input
-              name="email"
-              type="email"
-              placeholder="E-mail"
-              onChange={handleChange}
-              value={signupData.email}
-            ></input>
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              onChange={handleChange}
-              value={signupData.password}
-            ></input>
+            <div className="fourGearSignUpFormInput">
+              <input
+                name="fname"
+                placeholder="First Name"
+                autoComplete="off"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={signupData.fName}
+              ></input>
+              {errors.fname && (
+                <div className="signUpErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.fname}
+                </div>
+              )}
+            </div>
+            <div className="fourGearSignUpFormInput">
+              <input
+                name="lname"
+                placeholder="Last Name"
+                autoComplete="off"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={signupData.lName}
+              ></input>
+              {errors.lname && (
+                <div className="signUpErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.lname}
+                </div>
+              )}
+            </div>
+            <div className="fourGearSignUpFormInput">
+              <input
+                name="email"
+                type="email"
+                placeholder="E-mail"
+                autoComplete="off"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={signupData.email}
+              ></input>
+              {errors.email && (
+                <div className="signUpErrors">
+                  <CgDanger className="cgDanger" />
+                  {errors.email}
+                </div>
+              )}
+            </div>
+            <div className="fourGearSignUpFormInput">
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                autoComplete="off"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={signupData.password}
+              ></input>
+              {errors.password && <div className="signUpErrors"><CgDanger className="cgDanger" />{errors.password}</div>}
+            </div>
+
             <div className="fourGearSignupRegisterButton">
               <button>
                 {loading ? (
---
+++ src/schemas/signupValidation.jsx
@@ -0,0 +1,16 @@
+import * as Yup from "yup";
+
+const signUpValidation = Yup.object().shape({
+  fname: Yup.string().min(2,' First Name should be more than 2 Characters').max(20).required("Please enter your first name"),
+  lname: Yup.string().min(2,' Last Name should be more than 2 Characters').max(20).required("Please enter your last name"),
+  email: Yup.string().email(' Invalid Email').required("Please enter your email"),
+  password: Yup.string()
+    .min(8,'Password must be least 8 Characters')
+    .max(15)
+    .required("Password is required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      " Password must be at least 8 characters long and contain [A-Z,a-z,@#]"
+    ),
+});
+export default signUpValidation;
